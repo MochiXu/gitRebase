b91486049f26bce6e8552f78fa00419dd8fdf8ed
Mochi Xu <mirage_mc@icloud.com>
2022-06-05 21:24:23 +0800

    ztz changed2

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,9 @@
 public class Test {
     public static void main(String args[]){
-        //main init
-        System.out.println("main init");
-        //main
-        System.out.println("main commit 1");
-        // others changed the main brach
-        System.out.println("main commit 2");
-        System.out.println("others");
+        //main changed
+        while(true){
+            System.out.print("12");
+        }
 
     }
 }
