c09f745d6228be0284b61795ad1f3222dc7085fa
Mochi Xu <mirage_mc@icloud.com>
2022-06-05 19:48:42 +0800

    main commit 1

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,8 @@ public class Test {
     public static void main(String args[]){
         //main init
         System.out.println("main init");
+        //main
+        System.out.println("main commit 1");
 
     }
 }
