ed6eb097b5c314d99e4f8f59b970a459acfe91f8
MochiXu <mochix@moqi.ai>
2022-06-12 19:13:03 +0800

    dev1 -2 to -1 -rebase

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,28 @@
 public class Test {
     public static void main(String args[]){
-        for (int i=0;i<10;i++){
-            System.out.print("haha");
-        }
 
+        //main changed
+        int i = 0;
+        int count = 3 + i;
+        System.out.print("This is a log");
+
+        while (i < count) {
+            System.out.print("12");
+            System.out.print("This is a log");
+            i++;
+        }
+        System.out.print("This is a log");
+        while (1 >= count) {
+            System.out.print("12");
+            i--;
+            System.out.print("This is a log");
+            System.out.print("This is a log");
+
+            for (int k = 0; k < 10; k++) {
+                System.out.print("haha");
+            }
+            System.out.print("This is a log");
+            System.out.print("This is a log");
+        }
     }
 }
